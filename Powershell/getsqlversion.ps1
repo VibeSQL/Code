0fcf67467d8f1e7dd7645d6b05fbd7cef5ac50ef
VibeSQL <knatte@duck.com>
2025-10-31 13:10:09 +0100

    Added comments in code

--- Powershell/getsqlversion.ps1
+++ Powershell/getsqlversion.ps1
@@ -1,4 +1,15 @@
- # Path to your text file
+# Get SQL Server version
+# Reads hostnames from a file. Keep one hostname per row. 
+#
+# Output:
+#
+#   sqlserver01 - Version: 15.0.4445.1
+#   sqlserver02 - Version: 15.0.4445.1
+#   sqlserver03 - Version: 15.0.4445.1
+
+
+
+# Path to your text file
 $serverListPath = "C:\Temp\sqlservers.txt"
 
 # Load SMO (SQL Server Management Objects)
