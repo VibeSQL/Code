7f1b984676f9d01470cea9e5e9e11b1ce19c758b
VibeSQL <kdj@tutamail.com>
2026-01-29 20:07:49 +0100

    Changed som colors and added a title to the output.

--- bash/seccheck.sh
+++ bash/seccheck.sh
@@ -6,7 +6,7 @@
 #
 
 # Define the services and variables
-GREEN='\033[1;32m'
+PINK='\033[1;35m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
@@ -14,8 +14,10 @@ services=("mullvad-daemon" "firewalld" "NetworkManager" "sshd")
 sestatus=$(getenforce)
 relay=$(mullvad status |grep Relay |awk '{print $2}')
 
-
-if [ $sestatus != "Enforcing" ] 
+echo " Security Check Report"
+echo " ====="
+# Check SELinux status
+if [ $sestatus != "Enforcing" ];
 then
 	echo -e " Status: $RED Selinux not Enforcing${NC}"
 else
@@ -27,7 +29,7 @@ fi
 for service in "${services[@]}"; do
     if systemctl is-enabled "$service" &> /dev/null; 
 then
-    echo -e " Status: $service: ${GREEN}ok${NC}"
+    echo -e " Status: $service: ${PINK}ok${NC}"
 else
     echo -e " Status: $service: ${RED}not enabled${NC}"
 fi
@@ -37,7 +39,7 @@ echo " ====="
     if [ ! -z $relay ];
 then
     echo "VPN Connection info:"
-    echo -e " ${GREEN}$relay${NC}"
+    echo -e " ${PINK}$relay${NC}"
 else
     echo -e " ${RED}VPN not connected${NC}"
 fi
